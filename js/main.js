746e1ef61f23513c054379912cd7746ed191b5c9
Steffen Andre Hagen <steffen.hagen@knowit.no>
2021-01-15 17:31:50 +0100

    project files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const animContainer = document.getElementById('cta-btn');
+
+const animation = lottie.loadAnimation({
+    container: animContainer,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'data/v_01.json'
+});
+
+animation.addEventListener('data_ready', function() {
+    animContainer.addEventListener('click', function() {
+        animation.goToAndPlay(1);
+    });
+});
